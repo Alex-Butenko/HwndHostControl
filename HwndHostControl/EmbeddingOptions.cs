3233d71feb6cfe62e2366cd6f4c4d2d77022998d
Alex <butenko.alexey@gmail.com>
2012-07-29 15:13:20 +0800

    Удалены лишние references

--- HwndHostControl/EmbeddingOptions.cs
+++ HwndHostControl/EmbeddingOptions.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+//-----------------------------------------------------------------------------
 namespace HwndHostControl {
 	[Flags]
 	public enum EmbeddingOptions {
